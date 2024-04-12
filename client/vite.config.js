c60ccddde123c2fd4d7fdcf498e08f56487aa67c
JONAAVILA <jona_1969@hotmail.com>
2024-04-12 16:16:15 -0300

    up config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://JONAAVILA.github.io/drivers-in-react-and-redux",
 })
